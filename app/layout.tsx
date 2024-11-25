5429076e90aaf3e896363fbaad1193545dceac83
Alek <alek11221@gmail.com>
2024-11-24 21:24:56 -0500

    changed favicon to have absolute pathing

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,30 +19,30 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/favicon/favicon.ico',
+        url: './favicon/favicon.ico',
         sizes: 'any',
       },
       {
-        url: '/favicon/favicon-16x16.png',
+        url: './favicon/favicon-16x16.png',
         sizes: '16x16',
         type: 'image/png',
       },
       {
-        url: '/favicon/favicon-32x32.png',
+        url: './favicon/favicon-32x32.png',
         sizes: '32x32',
         type: 'image/png',
       },
     ],
-    shortcut: [{ url: '/favicon/favicon.ico' }],
+    shortcut: [{ url: './favicon/favicon.ico' }],
     apple: {
-      url: '/favicon/apple-touch-icon.png',
+      url: './favicon/apple-touch-icon.png',
       sizes: '180x180',
       type: 'image/png',
     },
     other: [
       {
         rel: 'manifest',
-        url: '/favicon/site.webmanifest',
+        url: './favicon/site.webmanifest',
       },
     ],
   },
