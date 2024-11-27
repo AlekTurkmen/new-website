b9287f6a7ab3f272103310aa6e725fd790baf08a
Alek <alek11221@gmail.com>
2024-11-27 13:53:50 -0500

    pre-stripe done

---
+++ app/onboarding/page.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Button } from "@/components/ui/shadcn/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/shadcn/card"
+import { Input } from "@/components/ui/shadcn/input"
+import { Label } from "@/components/ui/shadcn/label"
+import { Progress } from "@/components/ui/shadcn/progress"
+
+export default function TabsDemo() {
+  return (
+    <div className="min-h-screen w-full bg-black flex items-center justify-center px-4">
+        <Card className="w-[30%]">
+            <CardHeader>
+                <CardTitle>Step 4 of 4</CardTitle>
+                <Progress value={75} />
+            </CardHeader>
+            <CardContent className="space-y-2">
+                <div className="space-y-1">
+                <Label htmlFor="name">Name</Label>
+                <Input id="name" defaultValue="" />
+                </div>
+                <div className="space-y-1">
+                <Label htmlFor="email">Email</Label>
+                <Input id="email" defaultValue="" />
+                </div>
+                <div className="space-y-1">
+                <Label htmlFor="idealJob">Ideal Job</Label>
+                <Input id="idealJob" defaultValue="" />
+                </div>
+                <div className="space-y-1">
+                <Label htmlFor="idealCity">Ideal City</Label>
+                <Input id="idealCity" defaultValue="" />
+                </div>
+            </CardContent>
+            <CardFooter>
+                <a
+                  href="/onboarding"
+                  className="items-center gap-2 hover:underline hover:underline-offset-4 cursor-pointer"
+                >
+                  <Button>Save & Continue →</Button>
+                </a>
+            </CardFooter>
+        </Card>
+    </div>
+  )
+}
--- app/try/page.tsx
+++ app/try/page.tsx
@@ -3,8 +3,16 @@ import { LoginForm } from "@/components/login-form";
 
 export default function LoginPage() {
   return (
-    <div className="min-h-screen w-full bg-black flex items-center justify-center px-4">
+    <div className="min-h-screen w-full bg-black flex items-top justify-center px-4">
       <LoginForm />
+      <header className="row-start-1 gap-6 text-white flex-wrap items-center justify-left">  
+        <a
+          href="/onboarding"
+          className="items-center gap-2 hover:underline hover:underline-offset-4 cursor-pointer"
+        >
+          To Onboarding →
+        </a>
+      </header>
     </div>
   )
 } 
