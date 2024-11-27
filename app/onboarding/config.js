18199b6208dc9dc809aa906a1818262f842ea79e
Alek <alek11221@gmail.com>
2024-11-27 14:06:16 -0500

    adding stripe testing feature now

---
+++ app/onboarding/config.js
@@ -0,0 +1,3 @@
+window.config = {
+    stripe_key: 'pk_test_51QKO72JjYHX3jH1ON5Swkq7nwuRd0s1diVGvGIQHEDHoooaS2UqrSpDuoFloq1Y7KVfPVVrCtZ9RyYa24fifsYLC00MqxcBcWs'
+  };
--- app/onboarding/page.tsx
+++ app/onboarding/page.tsx
@@ -3,7 +3,6 @@ import { Button } from "@/components/ui/shadcn/button"
 import {
   Card,
   CardContent,
-  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
