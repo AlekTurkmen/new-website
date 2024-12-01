14669469161d2649b061d4543067cfdf37e85cdf
Alek <alek11221@gmail.com>
2024-12-01 16:26:14 -0500

    added secret keys, testing to see if that works

--- next.config.js
+++ next.config.js
@@ -4,10 +4,14 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  env: {
+    NEXT_PUBLIC_STRIPE_KEY: process.env.NEXT_PUBLIC_STRIPE_KEY,
+    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+    NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL,
+  },
   basePath: process.env.NODE_ENV === 'production' ? '/relevancy-landing' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/relevancy-landing' : '',
   trailingSlash: true,
-  reactStrictMode: true,
 }
 
 module.exports = nextConfig
