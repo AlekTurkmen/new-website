7302eb265c888f3974652310ec9524d3322a4cf8
alekturkmen <alek11221@gmail.com>
2025-03-26 00:57:05 -0400

    paused animation on hover

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import ShaderSphere, { ShaderSphereRef } from '@/components/Threejs/ShaderSphere/ShaderSphere';
 import Navbar from '@/components/ui/Navbar';
 import Footer from '@/components/ui/Footer';
-import { useRef, useEffect } from 'react';
+import { useRef, useCallback } from 'react';
 import localFont from 'next/font/local';
 import Link from 'next/link';
 
@@ -16,6 +16,18 @@ const boldonse = localFont({
 export default function Home() {
   const shaderSphereRef = useRef<ShaderSphereRef>(null);
 
+  const handleMouseEnter = useCallback(() => {
+    if (shaderSphereRef.current) {
+      shaderSphereRef.current.toggleAnimation(); // Pause animation
+    }
+  }, []);
+
+  const handleMouseLeave = useCallback(() => {
+    if (shaderSphereRef.current) {
+      shaderSphereRef.current.toggleAnimation(); // Resume animation
+    }
+  }, []);
+
   return (
     <>
       <div className={`${boldonse.variable} relative min-h-screen flex flex-col text-white font-lexend-regular`}>
@@ -24,7 +36,11 @@ export default function Home() {
         {/* Main Content Area */}
         <main className="flex-grow flex items-center justify-center">
           <Link href="/info">
-            <h1 className="text-5xl sm:text-6xl md:text-[8rem] font-boldonse max-w-[1600px] leading-tight text-red-100 sm:text-white opacity-100 hover:opacity-100 hover:text-red-600 transition-all duration-300">
+            <h1 
+              className="text-5xl sm:text-6xl md:text-[8rem] font-boldonse max-w-[1600px] leading-tight text-gray-400 sm:text-white opacity-100 hover:opacity-100 hover:text-red-600 transition-all duration-300"
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+            >
               Alek Turkmen
             </h1>
           </Link>
